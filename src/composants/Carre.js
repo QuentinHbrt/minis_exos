433e59a4bd88f90b49df42ea8ef1bbfbdb99561b
Quentin Hebert <QuentinHebert@MacBook-Air-de-Quentin.local>
2020-11-23 15:55:09 +0100

    🎉ADD: Mini exo > carre

---
+++ src/composants/Carre.js
@@ -0,0 +1,18 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+class Carre extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="carre"></div>
+                <button>On me voit</button>
+                <button>rouge</button>
+                <button>bleu</button>
+            </div>
+        );
+    }
+
+}
+
+export default Carre;
